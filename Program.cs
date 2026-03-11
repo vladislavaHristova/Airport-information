e09b7939c4f78b0870c3bb0b45f305975e0bc14b
vladislavaHristova <vladi210608@gmail.com>
2026-03-11 23:14:28 +0200

    Initial commit of Airport Information project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+public class Program {
+    public static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 6;
+
+        Console.WriteLine(a + b);
+    }
+}
---
+++ Tes/Tes/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Tes.Shared.Services;
+using Tes.Services;
+
+namespace Tes;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        // Add device-specific services used by the Tes.Shared project
+        builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
