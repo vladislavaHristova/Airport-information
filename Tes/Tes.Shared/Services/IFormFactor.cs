f384fb3fe03813ebde5dc0904c5acfe03e7d4a50
vladislavaHristova <vladi210608@gmail.com>
2026-04-10 08:16:32 +0300

    Delete Tes directory

--- Tes/Tes.Shared/Services/IFormFactor.cs
+++
@@ -1,7 +0,0 @@
-namespace Tes.Shared.Services;
-
-public interface IFormFactor
-{
-    public string GetFormFactor();
-    public string GetPlatform();
-}
--- Tes/Tes/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tes;
-
-public partial class MainPage : ContentPage
-{
-    public MainPage()
-    {
-        InitializeComponent();
-    }
-}
